573208b195191c346ed4380502945e764676fe58
Tony Davis <acdavis629@gmail.com>
2023-01-20 12:12:18 -0500

    refactor to build_model

--- deepchem/models/torch_models/pretrainer.py
+++ deepchem/models/torch_models/pretrainer.py
@@ -12,30 +12,35 @@ import os
 class ToyModel(nn.Module):
     def __init__(self, input_dim, d_hidden, d_output):
         super(ToyModel, self).__init__()
-        self._embedding = nn.Linear(input_dim, d_hidden)
-        self._head = nn.Linear(d_hidden, d_output)
-        self.model = nn.Sequential(self._embedding, self._head) 
+        embedding = nn.Linear(input_dim, d_hidden)
+        head = nn.Linear(d_hidden, d_output)
+        self.model = nn.Sequential(embedding, head) 
     def forward(self, x):
         return self.model(x)
     
 class ToyTorchModel(TorchModel):
     def __init__(self, input_dim, d_hidden, d_output, **kwargs): 
-        self.model = ToyModel(input_dim, d_hidden, d_output)
+        # self.model = ToyModel(input_dim, d_hidden, d_output)
         loss = L2Loss()
+        self.head = self.build_head(d_hidden, d_output)
+        self.embedding = self.build_embedding(input_dim, d_hidden)
+        self.model = self.build_model(self.embedding,self.head)
         super(ToyTorchModel, self).__init__(self.model, loss=loss, **kwargs) 
-    def get_head(self):
-        return self.model._head
-    def get_embedding(self):
-        return self.model._embedding
-        
+    def build_embedding(self, input_dim, d_hidden):
+        return nn.Linear(input_dim, d_hidden)
+    def build_head(self, d_hidden, d_output):
+        return nn.Linear(d_hidden, d_output)
+    def build_model(self,head,embedding):
+        return nn.Sequential(embedding, head)
 
 class Pretrainer(TorchModel): 
+    """does not modify internal state of model"""
     def __init__(self,
                  torchmodel:TorchModel,
                  **kwargs): 
         self.torchmodel = torchmodel
+        # self.model = torchmodel.model.get_embedding()
         self.pretrain_model_dir = os.path.join(torchmodel.model_dir, "pretrain")
-        # super().__init__(torchmodel.model, torchmodel.loss, model_dir=self.pretrain_model_dir,**kwargs) # unique pretrainer model dir 
         super().__init__(torchmodel.model, torchmodel.loss, model_dir=torchmodel.model_dir,**kwargs) # same model dir as torchmodel
         # self._pytorch_optimizer.freeze() ?
     def freeze_embedding(self):
@@ -44,29 +49,31 @@ class Pretrainer(TorchModel):
         self.torchmodel.model.embedding.weight.requires_grad = True
     def _define_pretrain_loss(self):
         return NotImplementedError("Subclass must define the pretrain loss")
-    def _swap_head(self): 
-        return NotImplementedError("Subclass must define how to change the head")
-    def fit(self,
-          dataset: Dataset,
-          nb_epoch: int = 10,
-          max_checkpoints_to_keep: int = 5,
-          checkpoint_interval: int = 1000,
-          deterministic: bool = False,
-          restore: bool = False,
-          variables: Optional[List[torch.nn.Parameter]] = None,
-          loss: Optional[LossFn] = None,
-          callbacks: Union[Callable, List[Callable]] = [],
-          all_losses: Optional[List[float]] = None) -> float:
+    # def _swap_head(self): 
+    #     return NotImplementedError("Subclass must define how to change the head")
+    # def fit(self,
+    #       dataset: Dataset,
+    #       nb_epoch: int = 10,
+    #       max_checkpoints_to_keep: int = 5,
+    #       checkpoint_interval: int = 1000,
+    #       deterministic: bool = False,
+    #       restore: bool = False,
+    #       variables: Optional[List[torch.nn.Parameter]] = None,
+    #       loss: Optional[LossFn] = None,
+    #       callbacks: Union[Callable, List[Callable]] = [],
+    #       all_losses: Optional[List[float]] = None) -> float:
         
-        """Fit the model to the data in dataset.
+    #     """Fit the model to the data in dataset.
         
-        Checkpoints saved at every interval will have the pretraining head of the model, not the original head.
-        """
-        self._swap_head()
-        super().fit(dataset, nb_epoch, max_checkpoints_to_keep, checkpoint_interval, deterministic, restore, variables, loss, callbacks, all_losses) #use assignment map to save only embedding layers
-        self._swap_head()
-        # self.save_checkpoint(model_dir = self.pretrain_model_dir)  # model_dir = self.pretrain_model_dir
-        torch.save(self.torchmodel.model.state_dict(), os.path.join(self.pretrain_model_dir, "checkpoint.pt"))
+    #     Checkpoints saved at every interval will have the pretraining head of the model, not the original head.
+    #     """
+        
+    #     # # self._swap_head()
+    #     # super().fit(dataset, nb_epoch, max_checkpoints_to_keep, checkpoint_interval, deterministic, restore, variables, loss, callbacks, all_losses) 
+    #     # # self._swap_head()
+    #     # # self._pytorch_optimizer.zero_grad() doesn't fix
+    #     # self.save_checkpoint(model_dir = self.pretrain_model_dir) 
+    #     # torch.save(self.torchmodel.model.state_dict(), os.path.join(self.pretrain_model_dir, "checkpoint.pt")) #works, skips the optimizer
 
 
 class ToyPretrainer(Pretrainer):
@@ -83,8 +90,9 @@ class ToyPretrainer(Pretrainer):
         return nn.Linear(self.embedding_dim, 2)
     def _swap_head(self):
         self.torchmodel.model._head = self.new_head
+        temp = self.new_head
         self.new_head = self.old_head
-        self.old_head = self.new_head
+        self.old_head = temp
     def _define_pretrain_loss(self):
         return L1Loss()
 
@@ -110,13 +118,14 @@ test_dataset = dc.data.NumpyDataset(X)
 toy = ToyTorchModel(input_size, hidden_layers, n_tasks, model_dir = './testfolder')
 toy2 = ToyTorchModel(input_size, hidden_layers, n_tasks, model_dir = './testfolder')
 
-### load_from_pretrained() 
 
-pretrainer = ToyPretrainer(toy2) # model_dir = './testfolder/pretrain'
+pretrainer = ToyPretrainer(toy) # model_dir = './testfolder/pretrain'
 pretrainer.fit(pt_dataset, nb_epoch=100, checkpoint_interval=10) 
+# MUST run load_from_pretrained() after fit, load from pretrained loads only embedding. two totally different models
 
-# toy.load_from_pretrained(pretrainer, include_top=True, model_dir = './testfolder/pretrain') # works if you run fit, otherwise not bc optimizer params different 
-toy.model.load_state_dict(torch.load(os.path.join(pretrainer.pretrain_model_dir, "checkpoint.pt")))  # works, checkpoint is output from pretrainer.fit
+### build new model, fit normally, then load from pretrained with only embedding
+toy.load_from_pretrained(pretrainer, include_top=True, model_dir = './testfolder/pretrain') # works if you run fit, otherwise not bc optimizer params different 
+# toy.model.load_state_dict(torch.load(os.path.join(pretrainer.pretrain_model_dir, "checkpoint.pt")))  # works, checkpoint is output from pretrainer.fit
 
 ### load_from_pretrained() test, works
 preds = toy2.predict(test_dataset)
