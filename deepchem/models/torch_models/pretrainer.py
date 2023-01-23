d62ef768557ccd44f40f36ac420761313f824d6b
Tony Davis <acdavis629@gmail.com>
2023-01-23 10:24:52 -0500

    remove nn.module

--- deepchem/models/torch_models/pretrainer.py
+++ deepchem/models/torch_models/pretrainer.py
@@ -33,7 +33,7 @@ class Pretrainer(TorchModel):
                  torchmodel:TorchModel,
                  **kwargs): 
         # self.pretrain_model_dir = os.path.join(torchmodel.model_dir, "pretrain")
-        super().__init__(torchmodel.model, torchmodel.loss,**kwargs) 
+        super().__init__(torchmodel, torchmodel.loss,**kwargs) 
     def freeze_embedding(self):
         self.torchmodel.model.embedding.weight.requires_grad = False
     def unfreeze_embedding(self):
@@ -41,19 +41,19 @@ class Pretrainer(TorchModel):
     def _define_pretrain_loss(self):
         return NotImplementedError("Subclass must define the pretrain loss")
 
-class ToyPretrainer(Pretrainer): # diamond inheritance    
+class ToyPretrainer(Pretrainer): 
     def __init__(self, 
                  model:ToyTorchModel,
                  pt_tasks:int,
                  **kwargs): 
         self.head = self.build_head(model.embedding.out_features, pt_tasks)
         self.embedding = model.embedding
-        self.model = model.build_model(model.embedding,model.head)
-        model.loss = self._define_pretrain_loss()
-        super().__init__(model, model_dir = model.model_dir, **kwargs)
+        self.model = model.build_model(self.embedding, self.head)
+        self.model.loss = self._define_pretrain_loss()
+        super().__init__(self.model, **kwargs)
     def _define_pretrain_loss(self):
         return L1Loss()
-    def build_head(self, d_hidden, pt_tasks): #can overwrite build_head
+    def build_head(self, d_hidden, pt_tasks): 
         return nn.Linear(d_hidden, pt_tasks)
 
 np.random.seed(123)
@@ -81,15 +81,12 @@ toy2 = ToyTorchModel(input_size, d_hidden, n_tasks, model_dir = './testfolder')
 
 pretrainer = ToyPretrainer(toy, pt_tasks=5) # model_dir = './testfolder/pretrain'
 pretrainer.fit(pt_dataset, nb_epoch=100, checkpoint_interval=10) 
-# MUST run load_from_pretrained() after fit, load from pretrained loads only embedding. two totally different models
 
 ### build new model, fit normally, then load from pretrained with only embedding
-toy2.load_from_pretrained(pretrainer, include_top=False, model_dir = './testfolder') # works if you run fit, otherwise not bc optimizer params different 
-# toy.model.load_state_dict(torch.load(os.path.join(pretrainer.pretrain_model_dir, "checkpoint.pt")))  # works, checkpoint is output from pretrainer.fit
+toy2.load_from_pretrained(pretrainer, include_top=False, model_dir = './testfolder') # works 
 
 ### load_from_pretrained() test, works
-# preds = toy2.predict(test_dataset)
-# print('toy2 preds: \n', preds)
-# # toy.load_from_pretrained(toy2, include_top=False, model_dir = toy2.model_dir)
-# preds = toy.predict(test_dataset)
-# print('toy preds: \n', preds)
+preds = toy2.predict(test_dataset)
+print('toy2 preds: \n', preds)
+preds = toy.predict(test_dataset)
+print('toy preds: \n', preds)
