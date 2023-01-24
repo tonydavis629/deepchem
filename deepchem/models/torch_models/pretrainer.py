c758bdb20ed3cdfdf77db2cd2e91956407e8e973
Tony Davis <acdavis629@gmail.com>
2023-01-23 20:07:15 -0500

    cleanup

--- deepchem/models/torch_models/pretrainer.py
+++ deepchem/models/torch_models/pretrainer.py
@@ -1,72 +1,70 @@
 import deepchem as dc
 from deepchem.models import TorchModel
 from deepchem.models.losses import L2Loss, L1Loss
-import torch
 import torch.nn as nn
 import numpy as np
-from deepchem.utils.typing import ArrayLike, LossFn, OneOrMany
-from deepchem.data import Dataset, NumpyDataset
-from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Tuple, Union
-import os
+
 
 class ToyTorchModel(TorchModel):
-    def __init__(self, 
-                 input_dim, 
-                 d_hidden, 
-                 d_output, 
-                 **kwargs):
+    """Example TorchModel for testing pretraining."""
+
+    def __init__(self, input_dim, d_hidden, d_output, **kwargs):
         self.input_dim = input_dim
-        self.d_hidden = d_hidden 
+        self.d_hidden = d_hidden
         self.d_output = d_output
         self.loss = L2Loss()
         self.head = self.build_head()
         self.embedding = self.build_embedding()
         self.model = self.build_model(self.embedding, self.head)
-        super().__init__(self.model, self.loss, **kwargs) 
+        super().__init__(self.model, self.loss, **kwargs)
+
     def build_embedding(self):
         return nn.Linear(self.input_dim, self.d_hidden)
+
     def build_head(self):
         return nn.Linear(self.d_hidden, self.d_output)
-    def build_model(self,embedding,head):
+
+    def build_model(self, embedding, head):
         return nn.Sequential(embedding, head)
 
-class Pretrainer(TorchModel): 
-    """does not modify internal state of model"""
-    def __init__(self,
-                 torchmodel:TorchModel,
-                 **kwargs): 
-        super().__init__(torchmodel.model, torchmodel.loss, **kwargs) 
+
+class Pretrainer(TorchModel):
+    """Abstract pretrainer class. This class is meant to be subclassed for pretraining TorchModels."""
+
+    def __init__(self, torchmodel: TorchModel, **kwargs):
+        super().__init__(torchmodel.model, torchmodel.loss, **kwargs)
+
     def freeze_embedding(self):
         self.torchmodel.model.embedding.weight.requires_grad = False
+
     def unfreeze_embedding(self):
         self.torchmodel.model.embedding.weight.requires_grad = True
+
     def build_head(self):
         return NotImplementedError("Subclass must define the head")
+
     def _define_pretrain_loss(self):
         return NotImplementedError("Subclass must define the pretrain loss")
-    
-        
-class ToyPretrainer(Pretrainer): 
-    def __init__(self, 
-                 model:ToyTorchModel,
-                 pt_tasks:int,
-                 **kwargs):
-        
+
+
+class ToyPretrainer(Pretrainer):
+    """Example Pretrainer for testing."""
+
+    def __init__(self, model: ToyTorchModel, pt_tasks: int, **kwargs):
+
         self.embedding = model.build_embedding()
-        self.head = self.build_head(model.d_hidden,pt_tasks)
-        
+        self.head = self.build_head(model.d_hidden, pt_tasks)
         self.model = model.build_model(self.embedding, self.head)
         self.loss = self._define_pretrain_loss()
-        
         torchmodel = TorchModel(self.model, self.loss, **kwargs)
-        
         super().__init__(torchmodel, **kwargs)
-        
+
     def _define_pretrain_loss(self):
         return L1Loss()
-    
-    def build_head(self, d_hidden, pt_tasks): 
+
+    def build_head(self, d_hidden, pt_tasks):
         return nn.Linear(d_hidden, pt_tasks)
+
 
 np.random.seed(123)
 n_samples = 10
@@ -87,19 +85,18 @@ X = np.random.rand(n_samples, input_size)
 y = np.random.randint(2, size=(n_samples, n_tasks)).astype(np.float32)
 test_dataset = dc.data.NumpyDataset(X)
 
-
-toy = ToyTorchModel(input_size, d_hidden, n_tasks, model_dir = './testfolder1')
+toy = ToyTorchModel(input_size, d_hidden, n_tasks, model_dir='./testfolder1')
 toy2 = ToyTorchModel(input_size, d_hidden, n_tasks)
 
-pretrainer = ToyPretrainer(toy, pt_tasks=5, model_dir = './testfolder2') 
-# pretrainer.fit(pt_dataset, nb_epoch=100, checkpoint_interval=10) 
+pretrainer = ToyPretrainer(toy, pt_tasks=5, model_dir='./testfolder2')
+pretrainer.fit(pt_dataset, nb_epoch=100, checkpoint_interval=10)
 
-### build new model, fit normally, then load from pretrained with only embedding
-toy2.load_from_pretrained(pretrainer, include_top=False, model_dir = './testfolder2') # works 
+toy2.load_from_pretrained(pretrainer,
+                          include_top=False,
+                          model_dir='./testfolder2')
 toy2.fit(ft_dataset, nb_epoch=100, checkpoint_interval=10)
 
 toy.fit(ft_dataset, nb_epoch=100, checkpoint_interval=10)
-### load_from_pretrained() test, works
 preds = toy2.predict(test_dataset)
 print('toy2 preds: \n', preds)
 preds = toy.predict(test_dataset)
