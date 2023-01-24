ecb852579a6123d1392634c0393fa048e90d67cf
Tony Davis <acdavis629@gmail.com>
2023-01-24 10:17:06 -0500

    building prototype

--- deepchem/alphafold.py
+++ deepchem/alphafold.py
@@ -1,28 +1,4 @@
-# %% [markdown]
-# ### Run a prediction with Alphafold
-# 
-# This notebook is created as a design document for making a prediction with alphafold in deepchem.  
 
-# %% [markdown]
-# #### Install dependencies
-# 
-# Extra dependencies for alphafold
-
-# %%
-%shell sudo apt install hmmer
-
-%shell conda install -y -q -c conda-forge -c bioconda \
-    kalign2=2.04 \
-    hhsuite=3.3.0 \
-    openmm=7.5.1 \
-    pdbfixer=1.7
-    
-%shell pip install -q \
-    ml-collections==0.1.0 \
-    PyYAML==5.4.1 \
-    py3dmol
-
-# %%
 sequence = 'MAAHKGAEHHHKAAEHHEQAAKHHHAAAEHHEKGEHEQAAHHADTAYAHHKHAEEHAAQAAKHDAEHHAPKPH'
 num_residues = len(sequence)
 
@@ -32,9 +8,9 @@ num_residues = len(sequence)
 # Use jackhmmr to identify homologous protein structures 
 
 # %%
-from deepchem.feat.sequence_featurizers.alphafold_featurizer import jackhmmer
+from deepchem.utils.alphafold_utils.jackhammer import Jackhmmer
 
-msas, deletion_matrices = jackhmmer(sequence, dbs = ['uniref90', 'smallbfd', 'mgnify'])
+msas, deletion_matrices = Jackhmmer(sequence, dbs = ['uniref90', 'smallbfd', 'mgnify'])
 
 # %% [markdown]
 # #### Process the features
@@ -57,53 +33,53 @@ processed_feature_dict = feat_pipe.process_features(feature_dict)
 # #### Create the model
 
 # %%
-from deepchem.models.layers import EvoformerLayer
-import torch
-from torch import nn
-from pytorch_lightning import LightningModule, Trainer, LightningDataModule
-ALPHAFOLD_PARAM_SOURCE_URL = 'https://storage.googleapis.com/alphafold/alphafold_params_2022-01-19.tar'
+# from deepchem.models.layers import EvoformerLayer
+# import torch
+# from torch import nn
+# from pytorch_lightning import LightningModule, Trainer, LightningDataModule
+# ALPHAFOLD_PARAM_SOURCE_URL = 'https://storage.googleapis.com/alphafold/alphafold_params_2022-01-19.tar'
 
-class EvoFormerStack(nn.Module):
-  """Stack of EvoFormer layers."""
+# class EvoFormerStack(nn.Module):
+#   """Stack of EvoFormer layers."""
 
-  def __init__(self, num_layers, num_heads, d_model, d_ff, dropout_rate):
-    super().__init__()
-    self.layers = nn.ModuleList([
-        EvoformerLayer(num_heads, d_model, d_ff, dropout_rate)
-        for _ in range(num_layers)
-    ])
+#   def __init__(self, num_layers, num_heads, d_model, d_ff, dropout_rate):
+#     super().__init__()
+#     self.layers = nn.ModuleList([
+#         EvoformerLayer(num_heads, d_model, d_ff, dropout_rate)
+#         for _ in range(num_layers)
+#     ])
 
-  def forward(self, x, msa, mask):
-    for layer in self.layers:
-      x = layer(x, msa, mask)
-    return x
+#   def forward(self, x, msa, mask):
+#     for layer in self.layers:
+#       x = layer(x, msa, mask)
+#     return x
 
-class AlphaFold(nn.Module): # or nn.module and create lightning wrapper?
-  def __init__(self,config):
-    """
-    Parameters:
+# class AlphaFold(nn.Module): # or nn.module and create lightning wrapper?
+#   def __init__(self,config):
+#     """
+#     Parameters:
     
-    config: dict
-      Model configuration
-    """
-  def download_alphafold_params(self, url=ALPHAFOLD_PARAM_SOURCE_URL):
-    """Downloads AlphaFold parameters from a URL."""
+#     config: dict
+#       Model configuration
+#     """
+#   def download_alphafold_params(self, url=ALPHAFOLD_PARAM_SOURCE_URL):
+#     """Downloads AlphaFold parameters from a URL."""
     
-  def iteration(self, feats, prevs, recycle=True):
-    """Runs a single iteration of the model."""
-    pass
-  def forward(self,batch):
-    """
-    Parameters:
+#   def iteration(self, feats, prevs, recycle=True):
+#     """Runs a single iteration of the model."""
+#     pass
+#   def forward(self,batch):
+#     """
+#     Parameters:
     
-    batch: dict
-      Batch of data
-    """
+#     batch: dict
+#       Batch of data
+#     """
     # extract data from batch
     # pass through model
     # recycle data
     # return output
-    pass
+    # pass
     
 
 
