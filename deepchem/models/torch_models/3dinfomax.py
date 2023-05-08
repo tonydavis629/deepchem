0ed6da8343028c19de66d051415e47f449079594
Tony Davis <acdavis629@gmail.com>
2023-05-08 11:02:14 -0400

    improts, test init

--- deepchem/models/torch_models/3dinfomax.py
+++ deepchem/models/torch_models/3dinfomax.py
@@ -1,4 +1,10 @@
 import torch
+from torch import nn
+from deepchem.feat.molecule_featurizers.conformer_featurizer import full_atom_feature_dims, full_bond_feature_dims, fourier_encode_dist
+from math import sqrt
+import dgl
+
+from typing import List, Union, Callable, Dict
 
 class AtomEncoder(torch.nn.Module):
 
@@ -754,24 +760,3 @@ class PNALayer(nn.Module):
         return {"e": self.pretrans(z2)}
 
 
-
-
-example_data = pd.read_csv(
-    '/home/tony/github/deepchem/deepchem/models/tests/assets/example_regression.csv'
-)
-
-# return list of SMILES strings
-ids = example_data['Compound ID'].tolist()
-datapoints = example_data['smiles'].tolist()
-targets = example_data['outcome'].tolist()
-
-graphs = featurize(datapoints)
-criterion = torch.nn.MSELoss()
-total_loss = 0
-for graph in graphs:
-    view2d = model2d(graph.to_dgl_graph())
-    view3d = model3d(graph.to_dgl_graph())
-    loss = criterion(view2d, view3d)
-    total_loss += loss
-
-print(total_loss)
